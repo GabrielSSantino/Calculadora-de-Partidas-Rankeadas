fc238bf3fa4f967d31e5e75f93777e25cebb8f90
GabrielSSantino <otakkoanimo@gmail.com>
2023-11-29 22:06:42 -0300

    Commit final

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Let inciando as vareaveis
 let victory = 50
 let ranker;
+
+//function score(), uma função com nome score que ira determinar o ranker
 
 function score() {
     if (victory < 10) {
@@ -19,15 +22,20 @@ function score() {
     }
 }
 
+//function winMatch(probability), uma função de probabilidade de caso ganhar ou perder
+
 function winMatch(probability) {
     console.log(`Você tem no total de partidas ganhas ${victory} e esta no nível ${ranker}`)
     console.log("A partida foi encontrada")
 
+    //Math.radom(); gerar um numero de 1 a 0
     const randomResult = Math.random();
+    //radomResult < probability; um numero gerado para radomResult e o 0.6 do probability se for menor true se não false
     const victoryMatch = randomResult < probability;
 
          if (victoryMatch) {
             victory++;
+            //Chamando a função score para atualizar o rank dependendo do resultado
             score();
             console.log(`O Herói tem de saldo de ${victory} está no nível de ${ranker}`);
          }else{
@@ -35,14 +43,13 @@ function winMatch(probability) {
             score();
             console.log(`O Herói perdeu tem de saldo de ${victory} está no nível de ${ranker}`);
          }
-         
+         //Retornando para saber se ganhou ou não
          return victoryMatch;
          
 }
 
-score();
-
+//Laço de repetição, ele ira repetir ate passar de 3
 for (let i = 0; i < 3; i++) {
     winMatch(0.6);
 }
-winMatch(0.6);
+winMatch(0.5);
